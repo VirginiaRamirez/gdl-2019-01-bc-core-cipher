9d4bcc446311c52ee2404d3a50e30b186562c4ad
VirginiaRamirez <mavirar@outlook.com>
2019-01-31 12:56:01 -0600

    Actualización del diseño en CSS

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-window.cipher = {
+// window.cipher = {
  
+  // ... 
+//};
 
-};
+
 function encode(){
     var resultado ="";
-    var desplaza = parseInt(document.getElementById("numero").value);
-    var texto = document.getElementById("contenido").value;
-    texto = String(texto).toUpperCase();
-    for (var i = 0; i < texto.length; i++) {
-        var codigo = (texto.charCodeAt(i) - 65 + desplaza) % 26 + 65;
+    var offset = parseInt(document.getElementById("numero").value);
+    var string = document.getElementById("contenido").value;
+    string = String(string).toUpperCase();
+    for (var i = 0; i < string.length; i++) {
+        var codigo = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
         resultado = resultado+String.fromCharCode(codigo);
     }
     document.getElementById("resultado").value=resultado;
 }
 
+
 function decode(){
     var resultado ="";
-    var desplaza = parseInt(document.getElementById("numero").value);
-    var texto = document.getElementById("contenido").value;
-    texto = String(texto).toUpperCase();
-    for (var i = 0; i < texto.length; i++) {
-        var codigo = (texto.charCodeAt(i) + 65 - desplaza) % 26 + 65;
+    var offset = parseInt(document.getElementById("numero").value);
+    var string = document.getElementById("contenido").value;
+    string = String(string).toUpperCase();
+    for (var i = 0; i < string.length; i++) {
+        var codigo = (string.charCodeAt(i) + 65 - offset) % 26 + 65;
         resultado = resultado+String.fromCharCode(codigo);
     }
     document.getElementById("resultado").value=resultado;
