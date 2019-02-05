3840ee1b5576534d7dc0e0b03c52ef74eb1b8b1c
VirginiaRamirez <mavirar@outlook.com>
2019-02-05 11:39:00 -0600

    Actual cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,25 @@
 window.cipher = {
- 
+
+    encode : (offset, string)=>{
+        let resultado ="";
+        offset = parseInt(document.getElementById("numero").value);
+        string = parseInt(document.getElementById("contenido").value);
+        string = String(string).toUpperCase();
+        for (let i = 0; i < string.length; i++) {
+            let codigo = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
+            resultado = resultado+String.fromCharCode(codigo);
+        }
+        document.getElementById("resultado").value=resultado;
+    },       
+     decode : (offset, string)=>{
+        let resultado ="";
+        offset = parseInt(document.getElementById("numero").value);
+        string = parseInt(document.getElementById("contenido").value);
+        string = String(string).toUpperCase();
+        for (let i = 0; i < string.length; i++) {
+            let codigo = (string.charCodeAt(i) + 65 - offset) % 26 + 65;
+            resultado =String.fromCharCode(codigo)+resultado;
+        }
+        document.getElementById("resultado").value=resultado;
+        }   
 };
